45036372324e866b17c65966d9fd0e95fa16ef1e
CletosMarcos <cleosmarcos@gmail.com>
2024-10-16 03:32:00 +0200

    implement kafka

--- order-service/src/main/java/com/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/microservices/order/service/OrderService.java
@@ -2,18 +2,22 @@ package com.microservices.order.service;
 
 import com.microservices.order.client.InventoryClient;
 import com.microservices.order.dto.OrderRequest;
+import com.microservices.order.event.OrderPlacedEvent;
 import com.microservices.order.model.Order;
 import com.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -28,6 +32,13 @@ public class OrderService {
 
 //        save Order to OrderRepository
             orderRepository.save(order);
+
+            // send the message to Kafka
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+
         } else {
             throw new RuntimeException("Product with the skuCode " + orderRequest.skuCode() + " is not in stock");
         }
